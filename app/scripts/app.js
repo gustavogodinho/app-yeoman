bf3758fec732a7bbd7dee6da519ec419482ee59e
Gustavo Gilberto Godinho <gustavo.godinho@ituran.com.br>
2019-09-30 17:33:15 -0300

    teste

---
+++ app/scripts/app.js
@@ -0,0 +1,14 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name appYeomanApp
+ * @description
+ * # appYeomanApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('appYeomanApp', []);
+
+
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name appYeomanApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the appYeomanApp
+ */
+angular.module('appYeomanApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
